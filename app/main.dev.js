cb13fc1e3c03d7f4a74110f1c17047b690ad6e80
Andreas Greimel <andreas@greimel.de>
2017-10-17 16:59:47 +0200

    Add dummy typings for redux persist, increase initial window size

--- app/main.dev.js
+++ app/main.dev.js
@@ -57,8 +57,8 @@ app.on('ready', async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 1280,
+    height: 800,
   });
 
   mainWindow.loadURL(`file://${__dirname}/app.html`);
