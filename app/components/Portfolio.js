43fa674d5b3dfeb985b50935fcb2df18e93f7c28
Andreas Greimel <andreas@greimel.de>
2017-11-04 13:30:43 +0100

    Show portfolio as stacked horizontal bar chart

--- app/components/Portfolio.js
+++ app/components/Portfolio.js
@@ -1,9 +1,14 @@
 // @flow
 import React, { Component } from 'react';
-import { Paper } from 'material-ui';
+import { Grid, Paper } from 'material-ui';
+import { defaults, HorizontalBar } from 'react-chartjs-2';
+import getColor from '../utils/colors';
 import type { Transaction } from '../reducers/transactions';
 import PortfolioPosition from './PortfolioPosition';
 import type { walletType } from '../reducers/wallets';
+
+// Disable animating charts by default.
+defaults.global.animation = false;
 
 type Props = {
   transactions: Transaction[],
@@ -20,20 +25,60 @@ export default class Portfolio extends Component<Props> {
     const sumBTC = calculateSum(ticker, portfolio, 'BTC');
     const sumEUR = calculateSum(ticker, portfolio, 'EUR');
 
+    const chartData = calculateChartData(ticker, portfolio, sumBTC);
+
     const keys = Object.keys(portfolio);
     return (
       <div>
         <Paper style={{ marginTop: 30, paddingBottom: 20, paddingTop: 10 }}>
-          <h1 style={{ paddingLeft: 20 }}>Portfolio</h1>
           {ticker.BTC ? (
-            <h3 style={{ paddingLeft: 20 }}>{sumBTC.toPrecision(5)} BTC
-              | {`${(ticker.BTC.BTC.CHANGEPCT24HOUR).toFixed(2)}%`}<br/>
-              {sumEUR.toPrecision(5)} €
-              | {`${(ticker.BTC.EUR.CHANGEPCT24HOUR).toFixed(2)}%`}
-            </h3>
+            <Grid container style={{ textAlign: 'center' }}>
+              <Grid item xs={2}/>
+              <Grid item xs={4}>
+                <h2 style={{ paddingLeft: 20 }}>
+                  {sumBTC.toPrecision(5)} BTC | {`${(ticker.BTC.BTC.CHANGEPCT24HOUR).toFixed(2)}%`}<br/>
+                </h2>
+              </Grid>
+              <Grid item xs={4}>
+                <h2>
+                  {sumEUR.toPrecision(5)} € | {`${(ticker.BTC.EUR.CHANGEPCT24HOUR).toFixed(2)}%`}
+                </h2>
+              </Grid>
+            </Grid>
           ) : ''}
+        </Paper>
+        <Paper style={{ marginTop: 30, paddingBottom: 20, paddingTop: 10 }}>
+          <HorizontalBar
+            data={chartData}
+            options={{
+              responsive: true,
+              maintainAspectRatio: false,
+              tooltips: {
+                mode: 'x',
+              },
+              legend: {
+                display: true,
+              },
+              scales: {
+                xAxes: [
+                  {
+                    stacked: true,
+                    ticks: {
+                      min: 0,
+                      max: 100,
+                    },
+                  }],
+                yAxes: [
+                  {
+                    stacked: true,
+                  }],
+              },
+            }}
+          />
+        </Paper>
+        <Paper style={{ marginTop: 30 }}>
           {keys
-            .filter(asset => coinlist[asset])
+            .filter(asset => ticker[asset])
             .sort((a, b) => (portfolio[b] * ticker[b].EUR.PRICE) - (portfolio[a] * ticker[a].EUR.PRICE))
             .map(asset => (
               <PortfolioPosition
@@ -106,4 +151,20 @@ function calculateSum(ticker: Object, portfolio: Object, currency: string) {
   return Object.keys(portfolio)
     .filter(asset => ticker[asset])
     .reduce((acc, asset) => acc + (ticker[asset][currency.toUpperCase()].PRICE * portfolio[asset]), 0);
+}
+
+function calculateChartData(ticker: Object, portfolio: Object, sumBTC: number) {
+  const datasets = Object.keys(portfolio)
+    .filter(asset => portfolio[asset] > 0)
+    .filter(asset => ticker[asset])
+    .sort((a, b) => (ticker[b].BTC.PRICE * portfolio[b]) - (ticker[a].BTC.PRICE * portfolio[a]))
+    .map(asset => ({
+      label: asset.toUpperCase(),
+      backgroundColor: getColor(asset),
+      borderColor: '#fff',
+      borderWidth: 1,
+      data: [((ticker[asset].BTC.PRICE * portfolio[asset] * 100) / sumBTC).toFixed(2)],
+    }));
+
+  return { datasets };
 }
