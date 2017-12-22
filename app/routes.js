49c6e14ffef4549e39c8e030979f5f52b63649cb
Andreas Greimel <andreas@greimel.de>
2017-12-22 15:34:53 +0100

    Move exchange components to pages/exchange

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,7 @@ import { Route, Switch } from 'react-router';
 import Layout from './containers/Layout';
 import HomePage from './pages/PortfolioPage';
 import WalletPage from './pages/WalletPage';
-import { ExchangePage } from './pages/ExchangePage';
+import { ExchangePage } from './pages/exchange/ExchangePage';
 import TransactionPage from './pages/TransactionPage';
 import SettingsPage from './pages/SettingsPage';
 import AboutPage from './pages/AboutPage';
