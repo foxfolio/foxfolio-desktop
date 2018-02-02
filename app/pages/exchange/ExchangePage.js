7fb05171ea6ba133dc899978cc8d690098783867
Andreas Greimel <andreas@greimel.de>
2018-02-02 17:14:17 +0100

    Add flow to ExchangePage

--- app/pages/exchange/ExchangePage.js
+++ app/pages/exchange/ExchangePage.js
@@ -1,7 +1,10 @@
+// @flow
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { ExchangeGrid } from './components/ExchangeGrid';
 import * as exchangeActions from '../../actions/exchanges';
+import type { GlobalState } from '../../reducers';
+import type { Exchanges } from '../../reducers/exchanges/types.d';
 
 function mapStateToProps(state: GlobalState): { exchanges: Exchanges } {
   return {
