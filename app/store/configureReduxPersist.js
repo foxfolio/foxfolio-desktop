92e892a73b752cee82a085da0f9f307d370d4fa7
Andreas Greimel <andreas@greimel.de>
2018-02-02 17:19:17 +0100

    Cleanup redux persist configuration

--- app/store/configureReduxPersist.js
+++ app/store/configureReduxPersist.js
@@ -4,32 +4,34 @@ import { createTransform, PersistConfig } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 export const configureReduxPersist = (): PersistConfig => {
-// $FlowFixMe
+  return {
+    key: 'primary',
+    blacklist: ['router', 'timer'],
+    transforms: [createDateTransform(), createExchangeTransform()],
+    storage,
+  };
+};
+
+const createExchangeTransform = () => createTransform(
+  (inboundState, key) => {
+    if (key === 'exchanges') {
+      return R.map(R.omit(['openRequests']))(inboundState);
+    }
+    return inboundState;
+  },
+);
+
+const createDateTransform = () => {
+  // $FlowFixMe
   const mapPath = R.curry((path, f, obj) => R.assocPath(path, f(R.path(path, obj)), obj));
 
   const convertDate = R.map(mapPath(['date'], dateString => new Date(dateString)));
   const convertDateIn = key => R.map(mapPath([key], convertDate));
 
-  const dateTransform = createTransform(null, (outboundState, key) => {
+  return createTransform(null, (outboundState, key) => {
     if (key === 'transactions' && Object.keys(outboundState).length > 0) {
       return R.pipe(convertDateIn('transfers'), convertDateIn('trades'))(outboundState);
     }
     return outboundState;
   });
-
-  const exchangeTransform = createTransform(
-    (inboundState, key) => {
-      if (key === 'exchanges') {
-        return R.map(R.omit(['openRequests']))(inboundState);
-      }
-      return inboundState;
-    },
-  );
-
-  return {
-    key: 'primary',
-    blacklist: ['router', 'timer'],
-    transforms: [dateTransform, exchangeTransform],
-    storage,
-  };
 };
