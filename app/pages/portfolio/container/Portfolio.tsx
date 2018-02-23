a92f47cc170bf3369ea800746ecdf80f5f71439e
Andreas Greimel <andreas@greimel.de>
2018-02-23 17:35:56 +0100

    Only show portfolio if ticker is available

--- app/pages/portfolio/container/Portfolio.tsx
+++ app/pages/portfolio/container/Portfolio.tsx
@@ -29,40 +29,42 @@ export default function PortfolioContainer({
   wallets,
   settings,
 }: PortfolioProps) {
-  const portfolio = calculatePortfolio(wallets, balances, settings);
-  const sum = {
-    crypto: calculateSum(ticker, portfolio.total, settings.cryptoCurrency),
-    fiat: calculateSum(ticker, portfolio.total, settings.fiatCurrency),
-  };
-  const change = {
-    crypto: calculateChange(ticker, portfolio.total, sum.crypto, settings.cryptoCurrency),
-    fiat: calculateChange(ticker, portfolio.total, sum.fiat, settings.fiatCurrency),
-  };
+  if (ticker && ticker[settings.cryptoCurrency]) {
+    const portfolio = calculatePortfolio(wallets, balances, settings);
+    const sum = {
+      crypto: calculateSum(ticker, portfolio.total, settings.cryptoCurrency),
+      fiat: calculateSum(ticker, portfolio.total, settings.fiatCurrency),
+    };
+    const change = {
+      crypto: calculateChange(ticker, portfolio.total, sum.crypto, settings.cryptoCurrency),
+      fiat: calculateChange(ticker, portfolio.total, sum.fiat, settings.fiatCurrency),
+    };
 
-  if (ticker[settings.cryptoCurrency] && !R.isEmpty(portfolio.total)) {
-    return (
-      <div>
-        <Paper style={{ marginTop: 0, paddingBottom: 25, paddingTop: 25, textAlign: 'center' }}>
-          <PortfolioHeader change={change} settings={settings} sum={sum} />
-        </Paper>
-        <Paper style={{ marginTop: 30, paddingBottom: 20, paddingTop: 10 }}>
-          <PortfolioChart
-            ticker={ticker}
-            portfolio={portfolio.total}
-            sum={sum.crypto}
-            settings={settings}
-          />
-        </Paper>
-        <Paper style={{ marginTop: 30 }}>
-          <PortfolioPositions
-            portfolio={portfolio}
-            ticker={ticker}
-            coinlist={coinlist}
-            settings={settings}
-          />
-        </Paper>
-      </div>
-    );
+    if (!R.isEmpty(portfolio.total)) {
+      return (
+        <div>
+          <Paper style={{ marginTop: 0, paddingBottom: 25, paddingTop: 25, textAlign: 'center' }}>
+            <PortfolioHeader change={change} settings={settings} sum={sum} />
+          </Paper>
+          <Paper style={{ marginTop: 30, paddingBottom: 20, paddingTop: 10 }}>
+            <PortfolioChart
+              ticker={ticker}
+              portfolio={portfolio.total}
+              sum={sum.crypto}
+              settings={settings}
+            />
+          </Paper>
+          <Paper style={{ marginTop: 30 }}>
+            <PortfolioPositions
+              portfolio={portfolio}
+              ticker={ticker}
+              coinlist={coinlist}
+              settings={settings}
+            />
+          </Paper>
+        </div>
+      );
+    }
   }
   return (
     <Paper style={{ marginTop: 0, paddingBottom: 25, paddingTop: 25, textAlign: 'center' }}>
