89669eec847b03035c5bb623f1b2c3f42c7a5c7d
Andreas Greimel <andreas@greimel.de>
2018-03-13 19:53:25 +0100

    Add NANO/XRB unification and colors (Fixes #7)

--- app/utils/unifySymbols.ts
+++ app/utils/unifySymbols.ts
@@ -2,6 +2,8 @@ export function unifySymbols(symbol: string): string {
   switch (symbol) {
     case 'XBT':
       return 'BTC';
+    case 'NANO':
+      return 'XRB';
     case 'ACT':
       return 'ACT*';
     default:
