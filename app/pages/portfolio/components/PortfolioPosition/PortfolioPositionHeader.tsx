ed630bf89c11a52b60d800849d496677b17ff8d2
Andreas Greimel <andreas@greimel.de>
2019-05-15 12:42:35 +0200

    #12 Improve number formatting of the portfolio position

--- app/pages/portfolio/components/PortfolioPosition/PortfolioPositionHeader.tsx
+++ app/pages/portfolio/components/PortfolioPosition/PortfolioPositionHeader.tsx
@@ -1,4 +1,5 @@
 import Typography from '@material-ui/core/Typography';
+import numeral from 'numeral';
 import * as React from 'react';
 
 export const PositionHeader = (name: string, quantity: number) => (
@@ -7,7 +8,7 @@ export const PositionHeader = (name: string, quantity: number) => (
       {name}
     </Typography>
     <Typography variant="body2" component="span" color="textSecondary">
-      {quantity.toPrecision(5)}
+      {numeral(quantity).format('0,0.[00000]')}
     </Typography>
   </div>
 );
